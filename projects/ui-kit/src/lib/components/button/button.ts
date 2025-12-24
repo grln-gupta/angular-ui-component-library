ff47ab6a473d52f7be100665ebab10d64afa2c13
guptagrln <guptagrln.sgec@gmail.com>
2025-12-25 01:27:34 +0530

    UI angular resuable components

---
+++ projects/ui-kit/src/lib/components/button/button.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-button',
+  standalone: true,
+  templateUrl: './button.html',
+  styleUrls: ['./button.css']
+})
+export class UiButtonComponent {}
---
+++ projects/ui-kit/src/lib/components/modal/modal.ts
@@ -0,0 +1,33 @@
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  HostListener
+} from '@angular/core';
+
+@Component({
+  selector: 'ui-modal',
+  standalone: true,
+  templateUrl: './modal.html',
+  styleUrls: ['./modal.css']
+})
+export class UiModalComponent {
+  @Input() open = false;
+  @Input() title = '';
+
+  @Output() close = new EventEmitter<void>();
+
+  @HostListener('document:keydown.escape')
+  onEsc() {
+    if (this.open) {
+      this.close.emit();
+    }
+  }
+
+  onBackdropClick(event: MouseEvent) {
+    if ((event.target as HTMLElement).classList.contains('ui-modal-backdrop')) {
+      this.close.emit();
+    }
+  }
+}
